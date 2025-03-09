3f9070b41be74ffc8f4099391ef7c05da6fb75c9
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-09 12:13:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const statusParagraph = document.getElementById('status');
+const enterButton = document.getElementById('enterBtn');
+
+enterButton.addEventListener('click', function() {
+	const newHeading = document.createElement('h1');
+	newHeading.textContent = 'Entered Metaverse';
+
+	statusParagraph.textContent = '';
+	statusParagraph.appendChild(newHeading);
+});
